edb6176a04d201bd0d5f8949d930e44501533aa0
Miha Bondarenko <limon18@ukr.net>
2022-07-14 17:17:45 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    adaptiveHeight:true,
+    autoplaySpeed: 5000,
+    arrows:false,
+    dots:true,
+    centerMode:true,
+  });
