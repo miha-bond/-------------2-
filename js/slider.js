beeca5239b67dfcd57fbb488ff0cabc7d80bf132
Miha Bondarenko <limon18@ukr.net>
2022-07-15 13:52:35 +0300

    3

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
 $('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    adaptiveHeight:false,
-    autoplaySpeed: 5000000,
-    arrows:false,
-    dots:true,
-    centerMode:true,
-  });
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  adaptiveHeight:false,
+  autoplaySpeed: 5000000,
+  arrows:false,
+  dots:true,
+  centerMode:true,
+});
